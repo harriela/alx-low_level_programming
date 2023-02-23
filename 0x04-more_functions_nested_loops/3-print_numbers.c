4bbfab4c8dc5d4980466b017a476c534b77022ce
harriela <harrietlakers@gmail.com>
2023-02-22 23:58:04 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print numbers from 0 up to 9
+ * Return: nothing on success
+ */
+
+void print_numbers(void)
+
+{
+
+	int start = 0, end = 9;
+
+	while (start <= end)
+	{
+		_putchar(start + '0');
+			start++;
+	}
+	_putchar('\n');
+}
